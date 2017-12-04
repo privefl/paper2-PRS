a71c2b26863d3edf136f18cddeb02ee9907531ef
privefl <florian.prive.21@gmail.com>
2017-12-04 17:03:25 +0100

    save

--- results1.R
+++ results1.R
@@ -1,84 +1,56 @@
-
 library(tidyverse)
 
-myggplot <- function(..., coeff = 1) {
-  bigstatsr:::MY_THEME(ggplot(...), coeff = coeff)
-} 
-
-top10 <- 1:110
-top20 <- 1:220
-
-results <- list.files("../thesis/paper2-PRS/results1/", full.names = TRUE) %>%
+results1 <- list.files("results1", full.names = TRUE) %>%
   map_dfr(~readRDS(.x)) %>%
-  as_tibble() %>%
-  mutate(
-    par.causal = factor(map_chr(par.causal, ~paste(.x[1], .x[2], sep = " in ")),
-                        levels = c("30 in HLA", paste(3 * 10^(1:3), "in all"))),
-    AUC = map_dbl(eval, ~bigstatsr::AUC(.x[, 1], .x[, 2])),
-    percCases10 = map_dbl(eval, ~mean(.x[order(.x[, 1], decreasing = TRUE)[top10], 2])),
-    percCases20 = map_dbl(eval, ~mean(.x[order(.x[, 1], decreasing = TRUE)[top20], 2]))
-  )
+  format_results()
 
-print(results, n = Inf)
-
-results %>%
-  filter(method == "T-Trees") %>%
-  pull(timing)
-
-results %>%
+results_ttrees <- results1 %>%
+  filter(method %in% c("logit-simple", "T-Trees")) %>%
   group_by_at(c(vars(starts_with("par")), "method")) %>%
-  summarise_at(c("timing", "nb.preds", "AUC", "percCases10"), mean)
+  summarise(
+    AUC_mean = mean(AUC), 
+    AUC_boot = boot(AUC, 1e5, mean), 
+    N = n(),
+    nb_preds_mean = mean(nb.preds),
+    timing_mean = mean(timing)
+  ) %>%
+  print(n = Inf)
 
-plot_results <- function(results, y, ylab = y) {
+p1 <- barplot_causal(results_ttrees, h2 = 0.8) +
+  facet_grid(par.model ~ par.dist) +
+  labs(y = "Mean of 5 AUCs")
+
+plot_ttrees <- function(results, y, ylab = y) {
   
-  dist <- "Distribution\nof effects"
-  
-  myggplot(results) +
-    geom_boxplot(aes_string("method", y, color = "par.dist", 
-                            fill = "par.dist"), alpha = 0.3) + 
+  results %>%
+    filter(method %in% c("logit-simple", "T-Trees")) %>%
+    myggplot() +
+    geom_boxplot(aes_string("par.causal", y, color = "method", 
+                            fill = "method"), alpha = 0.3) + 
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    facet_grid(par.model ~ par.causal) +
+    facet_grid(par.model ~ par.dist) +
     theme(strip.text.x = element_text(size = rel(2)),
           strip.text.y = element_text(size = rel(2))) +
-    labs(x = "Method", y = ylab, fill = dist, color = dist)
+    labs(x = "Causal SNPs (number and location)", y = ylab, 
+         fill = "Method", color = "Method") +
+    theme(legend.position = "none")
 }
 
-results %>%
-  filter(!grepl("PRS", method)) %>%
-  plot_results("timing", "Timing") +
-  scale_y_continuous(breaks = 0:10 * 2000, minor_breaks = NULL) 
-ggsave("timing.pdf", scale = 1/90, width = 1200, height = 900)
 
-plot_results(results, "AUC") +
-  scale_y_continuous(breaks = 0:10 / 10, minor_breaks = c(0:9 + 0.5) / 10)
-ggsave("AUC.pdf", scale = 1/90, width = 1200, height = 900)
-
-ggsave("perc-cases.pdf", scale = 1/90, width = 1200, height = 900)
-
-plot_results(results, "percCases20", "Percentage of cases in top 20%") +
-  scale_y_continuous(breaks = 0:10 / 10, minor_breaks = c(0:9 + 0.5) / 10)
-
-plot_results(results, "nb.preds", "Number of predictors") +
-  scale_y_log10(breaks = c(10^(0:7), 3 * 10^(0:7)), minor_breaks = NULL,
-                labels = scales::comma_format())
-ggsave("nb-preds.pdf", scale = 1/90, width = 1200, height = 900)
-
-ttrees_vs_logit <- filter(results, method %in% c("T-Trees", "logit-simple"))
-
-p_list <- list(
-  plot_results(ttrees_vs_logit, "timing", "Timing") +
-    scale_y_continuous(breaks = 0:10 * 2000, minor_breaks = NULL),
-  plot_results(ttrees_vs_logit, "nb.preds", "Number of predictors") +
-    scale_y_log10(breaks = c(10^(0:7), 3 * 10^(0:7)), minor_breaks = NULL,
-                  labels = scales::comma_format()),
-  plot_results(ttrees_vs_logit, "AUC") +
-    scale_y_continuous(breaks = 0:10 / 10, minor_breaks = c(0:9 + 0.5) / 10),
-  plot_results(ttrees_vs_logit, "percCases10", "Percentage of cases in top 10%") +
-    scale_y_continuous(breaks = 0:10 / 10, minor_breaks = c(0:9 + 0.5) / 10)
+p2 <- cowplot::plot_grid(
+  plot_ttrees(results1, "nb.preds", "Number of predictors"),
+  plot_ttrees(results1, "timing", "Execution time (in seconds)"),
+  ncol = 1, align = "hv", scale = 0.95, labels = LETTERS[2:3], label_size = 15
 )
-lapply(p_list, function(p) p + theme(legend.position = "none")) %>%
-  cowplot::plot_grid(plotlist = ., ncol = 2, align = "hv", scale = 0.9,
-                     labels = LETTERS[1:4], label_size = 25) %>%
-  cowplot::plot_grid(cowplot::get_legend(p_list[[1]]),
-                     rel_widths = c(1, 0.15))
+  
+p12 <- cowplot::plot_grid(
+  p1 + theme(legend.position = "none"), p2, 
+  ncol = 2, scale = 0.95, rel_widths = c(6, 4), labels = c("A", "")
+)
 
+cowplot::plot_grid(
+  cowplot::get_legend(p1 + theme(legend.direction = "horizontal")), p12, 
+  rel_heights = c(0.1, 1), ncol = 1
+)
+
+ggsave("figures/supp-ttrees.pdf", scale = 1/90, width = 1400, height = 1060)
--- results3.R
+++ results3.R
@@ -1,14 +1,107 @@
-# Put all results in a single tibble
-results3 <- list.files("results3", full.names = TRUE) %>%
-  map_dfr(~readRDS(.x)) %>%
-  as_tibble() %>%
-  mutate(
-    par.causal = factor(map_chr(par.causal, ~paste(.x[1], .x[2], sep = " in ")),
-                        levels = c("30 in HLA", paste(3 * 10^(1:3), "in all"))),
-    AUC = map_dbl(eval, ~bigstatsr::AUC(.x[, 1], .x[, 2]))
-  )
+library(tidyverse)
 
-results3 %>%
-  filter(par.h2 == 0.8) %>%
-  plot_results("AUC") +
-  scale_y_continuous(breaks = 0:10 / 10, minor_breaks = c(0:9 + 0.5) / 10)
+format_results <- function(results) {
+  results %>%
+    as_tibble() %>%
+    mutate(
+      par.causal = factor(map_chr(par.causal, ~paste(.x[1], .x[2], sep = " in ")),
+                          levels = c("30 in HLA", paste(3 * 10^(1:3), "in all"))),
+      AUC = map_dbl(eval, ~bigstatsr::AUC(.x[, 1], .x[, 2]))
+    )
+}
+
+results3 <- list.files(paste0("results", 2:3), full.names = TRUE) %>%
+  map_dfr(~readRDS(.x)) %>%
+  filter(method != "logit-triple", par.model != "fancy") %>%
+  format_results()
+
+
+results3.summary <- results3 %>%
+  group_by_at(c(vars(starts_with("par")), "method")) %>%
+  summarise(AUC_mean = mean(AUC), AUC_boot = boot(AUC), N = n()) %>%
+  print(n = Inf)
+
+## Make a function of this
+
+barplot_causal <- function(results, h2) {
+  
+  auc_max <- `if`(h2 == 0.8, 0.94, 0.84)
+  
+  results %>%
+    filter(par.h2 == h2) %>%
+    myggplot(aes(par.causal, AUC_mean, fill = method, color = method)) +
+    geom_hline(yintercept = 0.5, linetype = 2) +
+    geom_hline(yintercept = auc_max, linetype = 3, color = "blue") +
+    geom_bar(stat = "identity", alpha = 0.3, position = position_dodge()) +
+    geom_errorbar(aes(ymin = AUC_mean - 2 * AUC_boot, ymax = AUC_mean + 2 * AUC_boot),
+                  position=position_dodge(width=0.9), color = "black", width = 0.2) +
+    facet_grid(par.dist ~ .) + 
+    scale_y_continuous(limits = c(0.5, NA), minor_breaks = 0:20 / 20,
+                       oob = scales::rescale_none) +
+    theme(strip.text.x = element_text(size = rel(2)),
+          strip.text.y = element_text(size = rel(2)))+
+    labs(x = "Causal SNPs (number and location)", y = "Mean of 100 AUCs",
+         fill = "Method", color = "Method")
+}
+
+results3.summary %>%
+  filter(method %in% c("logit-simple", "PRS-max")) %>%
+  barplot_causal(h2 = 0.8) +
+  scale_fill_brewer(type = "qual", palette = 2) +
+  scale_color_brewer(type = "qual", palette = 2)
+
+ggsave("figures/main-AUC-logit.pdf", scale = 1/90, width = 844, height = 872)
+
+results3.summary %>%
+  filter(grepl("PRS", method)) %>%
+  barplot_causal(h2 = 0.8)
+
+ggsave("figures/main-AUC-PRS.pdf", scale = 1/90, width = 844, height = 872)
+
+results3.summary %>%
+  filter(method %in% c("logit-simple", "PRS-max")) %>%
+  barplot_causal(h2 = 0.5) +
+  scale_fill_brewer(type = "qual", palette = 2) +
+  scale_color_brewer(type = "qual", palette = 2)
+
+ggsave("figures/supp-AUC-logit.pdf", scale = 1/90, width = 844, height = 872)
+
+results3.summary %>%
+  filter(grepl("PRS", method)) %>%
+  barplot_causal(h2 = 0.5)
+
+ggsave("figures/supp-AUC-PRS.pdf", scale = 1/90, width = 844, height = 872)
+
+
+## Corr measures
+results3_corr <- results3 %>%
+  filter(method %in% c("logit-simple", "PRS-max")) %>%
+  mutate(
+    percCases10 = map_dbl(eval, ~mean(.x[order(.x[, 1], decreasing = TRUE)[1:110], 2])),
+    percCases20 = map_dbl(eval, ~mean(.x[order(.x[, 1], decreasing = TRUE)[1:220], 2])),
+    Parameters  = interaction(paste("h2 =", par.h2), par.dist, method, sep = " | ")
+  )
+  
+p1 <- results3_corr %>%
+  myggplot(aes(AUC, percCases10, color = Parameters)) + 
+  geom_point(size = 0.6, alpha = 0.5) +
+  geom_smooth(aes(linetype = Parameters), method = "lm", se = FALSE, size = 2) +
+  theme(legend.key.width = unit(3.5, "line"), 
+        legend.direction = "horizontal") + 
+  labs(y = "Proportion of cases among top 10% scores")
+p2 <- results3_corr %>%
+  myggplot(aes(AUC, percCases20, color = Parameters)) + 
+  geom_point(size = 0.6, alpha = 0.5) +
+  geom_smooth(aes(linetype = Parameters), method = "lm", se = FALSE, size = 2) + 
+  labs(y = "Proportion of cases among top 20% scores") +
+  theme(legend.position = "none")
+
+cowplot::plot_grid(p1 + theme(legend.position = "none"), p2 , 
+                   ncol = 2, align = "hv", scale = 0.95) %>%
+  cowplot::plot_grid(cowplot::get_legend(p1), ., rel_heights = c(0.1, 1), ncol = 1)
+
+
+ggsave("figures/supp-AUC-corr.pdf", scale = 1/90, width = 1558, height = 737)
+
+with(results3_corr, cor(AUC, percCases10))
+with(results3_corr, cor(AUC, percCases20))
