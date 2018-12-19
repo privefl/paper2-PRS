8ab1d64a00212f19f517cb339f9d0b67aa6e9bbc
Florian Privé <florian.prive@inp-grenoble.org>
2018-12-19 08:58:44 +0100

    sparSNP

--- sparSNP.R
+++ sparSNP.R
@@ -12,15 +12,18 @@ ind.train <- sort(sample(nrow(G), size = 12e3))
 ind.test <- setdiff(rows_along(G), ind.train)
 
 snp_writeBed(celiac, "backingfiles/celiac_train.bed", ind.row = ind.train)
-snp_writeBed(celiac, "backingfiles/celiac_test.bed", ind.row = ind.test)
+snp_writeBed(celiac, "backingfiles/celiac_test.bed",  ind.row = ind.test)
 
 Sys.setenv(PATH = paste0("~/Bureau/SparSNP:", Sys.getenv("PATH")))
 path <- setwd("~/Bureau/SparSNP")
 unlink("discovery", recursive = TRUE)
+
+# With NFOLDS=5 & NREPS=1
 system.time(
   system(sprintf("./crossval.sh %s sqrhinge",
                  file.path(path, "backingfiles/celiac_train")))
-)
+) # 3.2 h
 
-
-       
+system.time(
+  logit <- big_spLogReg(G, y[ind.train], ind.train, K = 5, ncores = 1)
+) # 38 sec  ~ 300 fold
